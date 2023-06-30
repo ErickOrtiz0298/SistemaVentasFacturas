35ae9dc3d40df156c7fd2542f0910e50506a01e5
ErickOrtiz0298 <a13003218@alumnos.uady.mx>
2023-06-30 15:07:40 -0600

    Primer Commit

---
+++ src/main/java/com/example/SistemaVentasFacturas/SistemaVentasFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaVentasFacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVentasFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVentasFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SistemaVentasFacturas/SistemaVentasFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaVentasFacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVentasFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
